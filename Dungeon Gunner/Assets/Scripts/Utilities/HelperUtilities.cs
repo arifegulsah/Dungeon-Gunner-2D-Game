2f7628c207abcd66577d615ac75358ea2671d838
Gülşah <69244976+arifegulsah@users.noreply.github.com>
2023-10-12 23:54:23 +0300

    helperUtilities_duzeltmelerYapildi

--- Dungeon Gunner/Assets/Scripts/Utilities/HelperUtilities.cs
+++ Dungeon Gunner/Assets/Scripts/Utilities/HelperUtilities.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public static class HelperUtilities 
 {
+
+    public static Camera mainCamera;
+
+    //mouseun locationu getiren method
+    public static Vector3 GetMouseWorldPosition()
+    {
+        if (mainCamera == null) mainCamera = Camera.main;
+
+        Vector3 mouseScreenPosition = Input.mousePosition;
+
+        //Clamp mouse posiiton to screen size? yani mouse ekranýn dýþýna çýkmýyor.
+        mouseScreenPosition.x = Mathf.Clamp(mouseScreenPosition.x, 0f, Screen.width);
+        mouseScreenPosition.y = Mathf.Clamp(mouseScreenPosition.y, 0f, Screen.height);
+
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(mouseScreenPosition);
+
+        worldPosition.z = 0f;
+
+        return worldPosition;
+
+    }
+
     public static bool ValidateCheckEmptyString(Object thisObject, string fieldName, string stringToCheck)
     {
         if(stringToCheck == "")
